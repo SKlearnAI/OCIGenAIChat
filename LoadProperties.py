53db25ff59e3786678f67fdfe7ab69d1497e7e18
AllianceSoftech <admin@theskillpedia.com>
2024-11-07 09:03:00 +0530

    Update LoadProperties.py

--- LoadProperties.py
+++ LoadProperties.py
@@ -14,6 +14,7 @@ class LoadProperties:
         self.compartment_ocid = js["compartment_ocid"]
         self.embedding_model_name=js["embedding_model_name"]
         self.langchain_key = js["langchain_key"]
+        self.oracle_23ai_password = js["oracle_23ai_password"]
         self.langchain_endpoint = js["langchain_endpoint"]
 
     def getModelName(self):
@@ -27,7 +28,10 @@ class LoadProperties:
 
     def getEmbeddingModelName(self):
             return self.embedding_model_name
-
+        
+    def getDBPassword(self):
+            return self.oracle_23ai_password
+        
     def getLangChainKey(self):
             return self.langchain_key
 
